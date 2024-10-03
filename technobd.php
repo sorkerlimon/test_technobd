5adcbda751036bdb8bcf4b7c346c31cffeff8382
Limon <sorkerlimon18@gmail.com>
2024-10-03 11:17:31 +0600

    database code update

---
+++ technobd.php
@@ -0,0 +1,25 @@
+<?php
+$host = 'your-rds-endpoint.amazonaws.com'; 
+$dbname = 'your_database_name';  
+$username = 'your_rds_username'; 
+$password = 'your_rds_password'; 
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name, email FROM users"; 
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
