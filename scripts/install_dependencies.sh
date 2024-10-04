b1473961fa39648120ff399cb08934d4bf8e5e46
Limon <sorkerlimon18@gmail.com>
2024-10-04 16:54:42 +0600

    final update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-# Update system and install Apache and PHP
-sudo yum update -y
-sudo yum install httpd -y
 sudo systemctl start httpd
-sudo systemctl enable httpd
 
-# Enable and install PHP 8.0
-sudo amazon-linux-extras enable php8.0
-sudo yum clean metadata
-sudo yum install php php-mysqlnd -y
 
--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-# Disable the default welcome page and restart Apache
-sudo mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.bak
-sudo systemctl restart httpd
 sudo systemctl status httpd
